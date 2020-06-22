49a389f82c9164108dbcd4de9df1fb44242aacc9
ahmad <akhrom@outlook.com>
2020-06-22 16:43:23 +0700

    update chapter4

--- chapter-4-how-to-use-variadic-parameter/main.go
+++ chapter-4-how-to-use-variadic-parameter/main.go
@@ -2,19 +2,23 @@ package main
 
 import "fmt"
 
-func main(){
-	some_numbs := hitung(5,5,5,5,5,5)
-	fmt.Println(fmt.Sprintf("Average your number is : %.2f",some_numbs))
+func main() {
+	var any_number1 int
+	var any_number2 int
+	fmt.Scanln(&any_number1)
+	fmt.Println("---------")
+	fmt.Scanln(&any_number2)
+	some_numbs := hitung(any_number1, any_number2)
+	fmt.Println(fmt.Sprintf("Average your number is : %.2f", some_numbs))
 }
 
+func hitung(number ...int) float64 {
 
-func hitung(number ...int) float64{
-
-var total int = 0
-for _, numbers := range number {		
-	total += numbers	 //dibaca total = total + number
+	var total int = 0
+	for _, numbers := range number {
+		total += numbers //dibaca total = total + number
 	}
-	
+
 	var avg = float64(total) / float64(len(number))
 	return avg
 }
