88014e460f9048a7bb3c86fa52fd2f3940aeda0e
ahmad <akhrom@outlook.com>
2020-07-25 12:50:31 +0700

    modified dir goslash

--- goslash/main.go
+++ goslash/main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/ahmadakhrom/go/goslash")
+)
 
 func main() {
-	d, _ := goslash.Sum(1,2)
+	d, _ := Sum(1, 2)
 	fmt.Println(d)
+}
+
+func Sum(i ...int) (int, error) {
+	summary := 0
+
+	for _, val := range i {
+		summary += val
+	}
+	return summary, nil
 }
--- goslash/operation_sum.go
+++ goslash/operation_sum.go
@@ -1,16 +1,10 @@
 package goslash
 
-import (
-	"fmt"
-	"log"
-)
-
-
-func sum(i ...int)(int, error) {
+func Sum(i ...int) (int, error) {
 	summary := 0
 
 	for _, val := range i {
 		summary += val
 	}
-return summary, nil
+	return summary, nil
 }
