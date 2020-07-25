86bfa484404e68ce78b8e9394eb97c7eb33f89c3
ahmad <akhrom@outlook.com>
2020-07-25 12:22:03 +0700

    add chapter 68 & modified dir goslash

---
+++ chapter-68-writing-documentation/go.mod
@@ -0,0 +1,3 @@
+module github.com/ahmadakhrom/go/chapter-68-writing-documentation
+
+go 1.14
--- chapter-68-writing-documentation/godoc.go
+++ chapter-68-writing-documentation/godoc.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/ahmadakhrom/go/goslash"
+)
+
+func main() {
+	val, _ := goslash.Sum(1, 2, 3, 4, 5)
+
+	fmt.Println(val)
 }
