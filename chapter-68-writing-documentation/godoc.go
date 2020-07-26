3840234a18a8839cfb187dffe94adf2be33c39a5
akhr0m <akhrom@outlook.com>
2020-07-26 12:57:12 +0700

    add chapter-69

--- chapter-68-writing-documentation/godoc.go
+++ chapter-68-writing-documentation/godoc.go
@@ -3,16 +3,14 @@ package main
 import (
 	"chapter-68-writing-documentation/godark"
 	"fmt"
-)
+//"g"
+	)
 
 func main() {
-	f, _ := godark.Times(1, 2)
+	f, _ :=godark.Sum(1, 2)
 	fmt.Println(f)
 
-	d, _ := godark.Sum(1, 3)
+	d, _ := godark.Times(1, 3)
 	fmt.Println(d)
-
-	turn_years, _ := godark.Years(12)
-	fmt.Println(turn_years, "dogs")
 
 }
---
+++ chapter-69-exercise-ninja-level-12/test1.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"chapter-69-exercise-ninja-level-12/godark"
+	"fmt"
+)
+
+func main() {
+	
+	turn_years, _ := godark.Years(12)
+	fmt.Println(turn_years, "dogs")
+
+}
