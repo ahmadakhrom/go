5fe68fbc4b501bfbe4ec66fd27a3736d0eb16745
ahmadakhrom <akhrom@outlook.com>
2020-09-19 01:13:33 +0700

    update

--- chapter-85-understanding-net-http-serverMux/03.defaultServeMux.go
+++ chapter-85-understanding-net-http-serverMux/03.defaultServeMux.go
@@ -4,30 +4,56 @@ import (
 	"io"
 	"net/http"
 )
+// ------------------------ bug example ------------------------
+//type person int
+//func (p person) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+//	io.WriteString(w, "hello person")
+//}
+//
+//type animal int
+//func (a animal) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+//	io.WriteString(w, "hello animal")
+//}
+//
+//type dfault int
+//func (d dfault) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+//	io.WriteString(w, "hello world")
+//}
 
-type person int
-func (p person) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+//func main()  {
+//	var p person
+//	var  a animal
+//	var d dfault
+//
+//	http.Handle("/",d)
+//	http.Handle("/person",p)
+//	http.Handle("/animal",a)
+//
+//	http.ListenAndServe(":8080",nil)
+//}
+
+func person(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "hello person")
 }
 
-type animal int
-func (a animal) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func animal(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "hello animal")
 }
 
-type dfault int
-func (d dfault) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "hello world")
+func dfault(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "hello dfault handle")
 }
 
 func main()  {
-	var p person
-	var  a animal
-	var d dfault
+	http.HandleFunc("/person",person)
+	http.HandleFunc("/animal",animal)
+	http.HandleFunc("/a",dfault)
 
-	http.Handle("/",d)
-	http.Handle("/person",p)
-	http.Handle("/animal",a)
+	//when you want to use just http.Handle, you can convert be like :
+	//http.Handle("/person",http.HandleFunc(person))
+	//http.Handle("/animal",http.HandleFunc(animal))
+	//http.Handle("/dfault",http.HandleFunc(dfault))
 
 	http.ListenAndServe(":8080",nil)
+
 }
