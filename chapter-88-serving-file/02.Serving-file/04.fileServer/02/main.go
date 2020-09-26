a55ad6988c237badddea02fbbda5d4d22173160e
akhr0m <akhrom@outlook.com>
2020-09-27 01:13:32 +0700

    update subdirectory chapter 88

--- chapter-88-serving-file/02.Serving-file/04.fileServer/02/main.go
+++ chapter-88-serving-file/02.Serving-file/04.fileServer/02/main.go
@@ -7,12 +7,17 @@ import (
 
 func main() {
 	http.HandleFunc("/", foo)
-	http.Handle("/resource/", http.StripPrefix("/resource",http.FileServer(http.Dir("./assets"))))
+	http.Handle("/resource/",  //setup direct to "/resource" which files have this path
+		http.StripPrefix("/resource",
+			http.FileServer(http.Dir("./assets"))))
 
 	http.ListenAndServe(":8080", nil)
 }
 
+
 func foo(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html;charset=utf-8")
+
+	//which path is begin prefix with "/resource" will be direct to directory assets
 	io.WriteString(w, `<img src = "/resource/sunset.jpg">`)
 }
